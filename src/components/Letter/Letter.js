2b79efb0afa152d917d86890e9b2c48b1b6c645a
Jordan <mcgalliard.jordan@gmail.com>
2022-08-29 14:51:50 -0600

    Make the shop grid have good aspect ratio. Add a menu for each item in the shop.

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import letterCSS from "./Letter.module.css"
 
@@ -6,14 +7,53 @@ function Letter(props) {
   let params = useParams();
   console.log(params.id)
 
-  
+  // This useEffect makes the sizes of all letters in their display boxes
+  // Based on the size of that box
+  // And also deals with changing the colors with the inputs
+  useEffect(() => {
+
+
+
+    // Get all imageArea, which is where the letter is displayed
+    let imageArea = Array.from(document.querySelectorAll(`.${letterCSS.imageArea}`));
+
+    // When page is resize, we get the width from the 1st imageArea
+    // Since they are all the same size.
+    // Then for each image area, we give the correct font-size
+    let handleResize = function() {
+
+      let imageAreaWidth = imageArea[0].offsetWidth;
+
+      for (let element of imageArea) {
+        element.style.fontSize = `${imageAreaWidth / 1.5}px`;
+      }
+    }
+    
+    // Run this one time on first render as well.
+    handleResize();
+    window.addEventListener('resize', handleResize);
+  },[])
 
   return (
-    <Link to={`../letter/${props.let}`}>
+    
       <div className={letterCSS.letterCard}>
-        <h1>Letter Card {props.let}</h1>
+        <div className={letterCSS.letterContent}>
+          <div className={letterCSS.imageArea}>
+            {props.let}
+          </div>
+          <div className={letterCSS.titleArea}>
+            <Link to={`../letter/${props.let}`}>
+              <p>The letter {props.let}</p>
+            </Link>
+            <div className={letterCSS.colorInputs}>
+              <input type="color"></input>
+              <input type="color"></input>
+            </div>
+            <button>+</button>
+          </div>
+        </div>
       </div>
-    </Link>
+   
   )
 }
 
