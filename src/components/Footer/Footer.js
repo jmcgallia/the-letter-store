9983884be6d46ccfd057d687c424ae1b4e278f49
Jordan <mcgalliard.jordan@gmail.com>
2022-08-30 20:57:35 -0600

    Update footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import footerCSS from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={footerCSS.footer}>
-      <h1>This is the footer.</h1>
+      <p><a href="https://github.com/jordan-mcgalliard/the-letter-store">@jordan-mcgalliard</a></p>
+      <p>Images from <a href="https://www.pexels.com/license/">Pexels.com</a></p>
     </footer>
   )
 }
